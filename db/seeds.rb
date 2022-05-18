6420c0651e99239daf9f9a3566b0923b9266ebc7
Antoine Thiran <antoinethiran@hotmail.com>
2022-05-18 14:56:50 +0200

    Adding stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+country = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: country.sample)
+end
